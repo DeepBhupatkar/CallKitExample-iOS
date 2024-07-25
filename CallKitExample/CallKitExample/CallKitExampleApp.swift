206261793c937f0926f47c4dfc41d1af711da1d7
Deep Bhupatkar <bhupatkardeep@gmail.com>
2024-07-25 16:23:08 +0530

    Initial Commit

---
+++ CallKitExample/CallKitExample/CallKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallKitExampleApp.swift
+//  CallKitExample
+//
+//  Created by Deep Bhupatkar on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CallKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
